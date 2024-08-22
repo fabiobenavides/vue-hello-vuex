6891179ea099b198fdfe0fa503fff3b73a07e7ec
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-08-22 14:43:04 -0600

    Actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,34 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
     state: {
         counter: 0
     },
     mutations: {
-        increaseCounter(state) {
-            state.counter++;
-          },
-          decreaseCounter(state) {
-            state.counter--;
-          }
+        increaseCounter(state, randomNumber) {
+            state.counter += randomNumber;
+        },
+        decreaseCounter(state, randomNumber) {
+            state.counter -= randomNumber;
+        }
     },
     actions: {
-
+        increaseCounter({commit}) {
+            axios('https://www.random.org/integers/?num=1&min=1&max=9&col=1&format=plain&base=10')
+                .then(response => {
+                    commit('increaseCounter', response.data);
+                });
+        },
+        decreaseCounter({commit}) {
+            axios('https://www.random.org/integers/?num=1&min=1&max=9&col=1&format=plain&base=10')
+                .then(response => {
+                    commit('decreaseCounter', response.data);
+                });
+        }
     },
     getters: {
-
+        
     },
     modules: {
 
